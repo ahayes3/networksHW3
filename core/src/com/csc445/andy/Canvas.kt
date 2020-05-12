edc09bb86ce148e43ada540741396e0ac97760f1
andrew hayes <ahayes5@oswego.edu>
2020-05-12 17:41:58 -0400

    common input finished began creating Tools drawing pixmap corrected

--- core/src/com/csc445/andy/Canvas.kt
+++ core/src/com/csc445/andy/Canvas.kt
@@ -1,6 +1,7 @@
 package com.csc445.andy
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputMultiplexer
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.*
 import com.badlogic.gdx.scenes.scene2d.Stage
@@ -9,30 +10,39 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport
 class Canvas(val app:DrawingApp,val resX:Int,val resY:Int): Screen {
 	private val batch = app.batch
 	private val clearColor = Color.LIGHT_GRAY
-	private val pixmap = Pixmap(resX,resY,Pixmap.Format.RGB888)
-	private val camera = OrthographicCamera()
+	val pixmap = Pixmap(resX,resY,Pixmap.Format.RGB888)
+	val camera = OrthographicCamera()
+	val multiplexer = InputMultiplexer()
 	private val stage = Stage(ScreenViewport())
 	private var texture:Texture
 	init {
-		Gdx.input.inputProcessor = MyInputProcessor()
-		camera.setToOrtho(false, resX.toFloat(),resY.toFloat())
+		multiplexer.addProcessor(CommonInput(this))
+		
+		
+		Gdx.input.inputProcessor = CommonInput(this)
+		camera.setToOrtho(false, Gdx.graphics.displayMode.width.toFloat(), Gdx.graphics.displayMode.height.toFloat())
 		pixmap.setColor(Color.WHITE)
 		pixmap.fill()
 		texture = Texture(pixmap)
+		camera.position.x = texture.width/2F
+		camera.position.y = texture.height/2F
 	}
 	override fun hide() {}
 	
-	override fun show() {}
+	override fun show() {
+		reposition()
+	}
 	
 	override fun render(delta: Float) {
-		
 		
 		//drawing
 		Gdx.gl.glClearColor(clearColor.r,clearColor.g,clearColor.b,clearColor.a)
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 		batch.projectionMatrix = camera.combined
 		
+		texture = Texture(pixmap)
 		batch.begin()
+		texture.draw(pixmap,0,0)
 		batch.draw(texture,0F,0F)
 		batch.end()
 		
@@ -45,10 +55,17 @@ class Canvas(val app:DrawingApp,val resX:Int,val resY:Int): Screen {
 	
 	override fun resume() {}
 	
-	override fun resize(width: Int, height: Int) {}
+	override fun resize(width: Int, height: Int) {
+		camera.viewportWidth = width.toFloat()
+		camera.viewportHeight = height.toFloat()
+	}
 	
 	override fun dispose() {
 		pixmap.dispose()
+	}
+	
+	fun reposition() {
+	
 	}
 	
 }
---
+++ core/src/com/csc445/andy/CommonInput.kt
@@ -0,0 +1,55 @@
+package com.csc445.andy
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.InputProcessor
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.math.Vector3
+
+class CommonInput(val canvas:Canvas) : InputProcessor {
+	private var rightDown = false
+	
+	override fun mouseMoved(screenX: Int, screenY: Int): Boolean {
+		if(rightDown) {
+			val unprojected = canvas.camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0F))
+			canvas.camera.position.x -= unprojected.x
+			canvas.camera.position.y -= unprojected.y
+		}
+		return false
+	}
+	
+	override fun keyTyped(character: Char): Boolean {
+		return false
+	}
+	
+	override fun scrolled(amount: Int): Boolean {
+		canvas.camera.zoom += amount*3*Gdx.graphics.getDeltaTime();
+		if(canvas.camera.zoom <0.05)
+			canvas.camera.zoom = 0.05f;
+		return true
+	}
+	
+	override fun keyUp(keycode: Int): Boolean {
+		return false
+	}
+	
+	override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
+		return false
+	}
+	
+	override fun keyDown(keycode: Int): Boolean {
+		return false
+	}
+	
+	override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+		if(Input.Buttons.RIGHT == button)
+			rightDown = true
+		return false
+	}
+	override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+		if(Input.Buttons.RIGHT == button)
+			rightDown = false
+		return false
+	}
+	
+}
